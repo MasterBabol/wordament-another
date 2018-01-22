1d50ac9e8a2ec90e74a0d5b2abadbc60b38a18b5
Babol <babol@live.co.kr>
2018-01-23 00:27:55 +0900

    Initial release

---
+++ wscore.php
@@ -0,0 +1,27 @@
+<?php
+
+$data = array('totalsum'=>0, 'totalgames'=>0, 'best'=>0);
+
+$ifp = fopen("wscore.dat", "r");
+if ($ifp)
+{
+	$user_score = $_GET['score'];
+	$readstr = fgets($ifp);
+	fclose($ifp);
+	$data = unserialize($readstr);
+	$data['totalsum'] += $user_score;
+	$data['totalgames'] += 1;
+	if ($user_score > $data['best'])
+		$data['best'] = $user_score;
+}
+
+$writestr = serialize($data);
+$ofp = fopen("score.dat", "w");
+if ($ofp)
+{
+	fputs($ofp, $writestr);
+	fclose($ofp);
+}
+echo json_encode($data);
+
+?>
